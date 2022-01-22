ee856065d2767ffb5cc53c72511320954cd31b55
Jack Tsai <64248906+JackCobra11@users.noreply.github.com>
2022-01-22 04:24:23 +0000

    Update BidHelper.py

--- BidHelper.py
+++ BidHelper.py
@@ -28,6 +28,13 @@ class calibration(): # 校正
         time.sleep(OPERATION_DELAY)
         return self.pos_now
 
+    def input(self):
+        print("按下 *i* 确认当前鼠标位置为 *输入验证码区域* 坐标：")
+        keyboard.wait('i')  # 等待输入 I
+        self.pos_now = PyMouse().position()
+        print(self.pos_now)
+        time.sleep(OPERATION_DELAY)
+        return self.pos_now
 
 class OperationPart():
     def Bid(self):
@@ -37,6 +44,8 @@ class OperationPart():
         while True:
             keyboard.wait('s') # 等待输入 S
             auto.click(BID_POS) # 按S后自动点击出价键
+            time.sleep(OPERATION_DELAY)
+            auto.click(INPUT_POS) # 在延时后自动点击输入验证码区域
             time.sleep(OPERATION_DELAY)
 
     def Submit(self):
@@ -50,10 +59,14 @@ class OperationPart():
 
 if __name__ == "__main__":
     BID_POS = calibration().bid()
+    INPUT_POS = calibration().input()
     SUBMIT_POS = calibration().submit()
     print("Press *S* to bid the price!")
+    # print("Press *I* to Input the price!")
     print("Press *Enter* to submit the price!")
     t1 = threading.Thread(target=OperationPart.Bid, args=("t1",))
     t2 = threading.Thread(target=OperationPart.Submit, args=("t2",))
+    # t3 = threading.Timer(5, OperationPart.Submit)
     t1.start()
     t2.start()
+    # t3.start()
